1f0ac00f79d20bfec40edd611a48d6b41d4a5bcd
lee <nhattruongta13@gmail.com>
2026-05-03 23:41:32 -0500

    longer hash

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     dually = db.Column(db.Integer, default=0)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
